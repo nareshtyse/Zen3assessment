2f393270bb2fb7a351de29df4b43bc217b298fae
nareshtyse <48803828+nareshtyse@users.noreply.github.com>
2021-05-13 13:09:52 -0400

    Update deploy.ps1

--- CreateVMinAzure/deploy.ps1
+++ CreateVMinAzure/deploy.ps1
@@ -6,6 +6,6 @@ Set-AzContext -Subscription $subscriptionId
 $rgExists = Get-AzResourceGroup -Name $resourceGroup -ErrorAction SilentlyContinue
 if($null -ne $rgExists){
     Write-Host "Deploying Virtual Machine under resource group $resourceGroup" -ForegroundColor Green
-    #New-AzResourceGroupDeployment -Name sample -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parametersFile -Verbose
-	New-AzResourceGroupDeployment  -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parametersFile -Verbose
+    New-AzResourceGroupDeployment -Name VMcreation -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $parametersFile -Verbose
+	
 }
